fb081e58b63c59467950a0d19b019603dfaf32a3
Tim Pellissier <timpellissier@gmail.com>
2016-05-01 15:10:21 -0500

    Remove desc flag and simplify comments
    Using the desc flag to sort by descending proved to be slower and less
    readable than just reversing a list sorted ascending. Also, my comments
    were a bit overboard and took more work to update than the code itself,
    which is probably not good.

--- merge-sort/merge-sort.py
+++ merge-sort/merge-sort.py
@@ -1,7 +1,7 @@
 import time
 import sys
 
-def sort(arr, desc):
+def sort(arr):
 	l = len(arr)
 
 	# If the array is too large, split it in two and recursively call sort on each half
@@ -10,30 +10,28 @@ def sort(arr, desc):
 		arr1 = arr[:l/2]
 		arr2 = arr[l/2:]
 
-		return merge(sort(arr1, desc), sort(arr2, desc), desc)
+		return merge(sort(arr1), sort(arr2))
 
 	# If the array is down to two elements:
-	# If the descending flag is false, return a reversed version if the first element is bigger
-	# If the descending flag is true, return reversed array if *second* element is bigger
+	# Return a reversed version if the first element is bigger
 	# Otherwise return the array unchanged
 	elif l == 2:
-		if (arr[0] > arr[1] and not desc) or (arr[0] < arr[1] and desc):
+		if (arr[0] > arr[1]):
 			return [arr[1], arr[0]]
 		else:
 			return arr
 
-	# If the array has just one element, return itself
 	else:
 		return arr
 
-def merge(arr1, arr2, desc):
+def merge(arr1, arr2):
 	# Set array traversal pointers to 0
 	i, j = 0, 0
 	len_1, len_2 = len(arr1), len(arr2)
 	result = []
 
 	while 1:
-		# If the end of one array is reached, complete the merged array with the rest of the other array
+		# If the end of one array is reached, append the rest of the other array
 		if i == len_1:
 			result.extend(arr2[j:])
 			break
@@ -42,9 +40,8 @@ def merge(arr1, arr2, desc):
 			break
 
 		# Compare values at each pointer
-		# Append the lower value (if desc is false) or the higher value (if desc is true) to the merged array
-		# Increment the appropriate pointer
-		if (arr1[i] < arr2[j] and not desc) or (arr1[i] > arr2[j] and desc):
+		# Append the lower value to the merged array
+		if (arr1[i] < arr2[j]):
 			result.append(arr1[i])
 			i += 1
 		else:
@@ -56,15 +53,15 @@ def merge(arr1, arr2, desc):
 def main():
 	start_array = [n for n in reversed(range(256000))]
 
-	descending = False
-
 	start_time = time.time()
 
-	# If 'd' or 'D' is specified in command line, switch on the 'sort descending' flag
+	# If 'd' or 'D' is specified in command line, reverse result
 	if len(sys.argv) > 1 and sys.argv[1] in ['d', 'D']:
-		descending = True
+		sorted_array = list(reversed(sort(start_array)))
 
-	sort(start_array, descending)
+	else:
+		sorted_array = sort(start_array)
+
 	print time.time() - start_time
 
 if __name__ == '__main__':
