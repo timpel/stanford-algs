5748265d5102ee69e928d65ff3d40779af120dac
Tim Pellissier <timpellissier@gmail.com>
2016-05-07 13:22:07 -0500

    Implement merge component of merge_and_count_split
    The merging part of merge_and_count_split was is taken right out
    of merge-sort. A test revealed a mistake in merge_and_sort where
    0 was returned instead of the array when the array has one
    element, so that was fixed.

--- count-inversions/count_inversions.py
+++ count-inversions/count_inversions.py
@@ -5,7 +5,7 @@ def sort_and_count(arr):
 	n = len(arr)
 
 	if n == 1:
-		return 0
+		return arr
 
 	else:
 		first_half = arr[:n/2]
@@ -14,7 +14,25 @@ def sort_and_count(arr):
 
 
 def merge_and_count_split(arr1, arr2):
-	return 0
+	i, j = 0, 0
+	result = []
+
+	while 1:
+		if i == len(arr1):
+			result.extend(arr2[j:])
+			break
+		if j == len(arr2):
+			result.extend(arr1[i:])
+			break
+
+		if (arr1[i] < arr2[j]):
+			result.append(arr1[i])
+			i += 1
+		else:
+			result.append(arr2[j])
+			j += 1
+
+	return result
 
 def main(arr_len):
 	test_arr = [randint(0,arr_len) for n in range(arr_len)]
