735f4cb1c0fffb6b0dd7528b46a13f719dbf5677
Tim Pellissier <timpellissier@gmail.com>
2016-05-07 23:11:39 -0500

    Implement multiplication by naive algorithm
    This commit completes the implementation of the O(n^2) implementation
    of 2-integer muliplication. Digits are carried when necessary and the
    final 'addition' step of the algorithm is done by keeping a running
    sum of each indiviual product multiplied by an appropriate 'tens'
    multiplier.

--- integer-multiplication/naive.c
+++ integer-multiplication/naive.c
@@ -14,24 +14,31 @@ int* toInt(char * s, int len){
 }
 
 int main(int argc, char *argv[]) {
-	int i, j, product;
+	int i, j, product, carry, shift;
 	int result = 0;
+	int shift_start = 1;
 	int n1_len = strlen(argv[1]);
 	int n2_len = strlen(argv[2]);
 
 	int * n1 = toInt(argv[1], n1_len);
 	int * n2 = toInt(argv[2], n2_len);
 
-	for(j = n2_len - 1; j >= 0; j--){
-		for(i = n1_len - 1; i >= 0; i--){
-			product = n1[i] * n2[j];
-			if (product > 9) {
-				if (j > 0) {
-					/* carry digits */
-				}
+	for(j = n2_len - 1; j >= 0; j--, shift_start *= 10){
+		carry = 0;
+		for(i = n1_len - 1, shift = shift_start; i >= 0; i--, shift *= 10){
+			product = n1[i] * n2[j] + carry;
+			if (product > 9 & i > 0) {
+				carry = product / 10;
+				product = product % 10;
 			}
+			else {
+				carry = 0;
+			}
+			result += product * shift;
 		}
 	}
+
+	printf("%d\n", result);
 
 	free(n2);
 	free(n1);
