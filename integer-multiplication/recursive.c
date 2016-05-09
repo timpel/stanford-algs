89c2228b39f34c7546655760de7299e073961078
Tim Pellissier <timpellissier@gmail.com>
2016-05-08 21:51:28 -0500

    Implement recursive multiplication for some inputs
    It works... but only on inputs with an equal number of digits,
    and the number of digits must be a power of 2. Basically it
    can multiply two 2-digit or two 4-digit numbers.
    
    Removed the ill-conceived getabcd function and replaced with
    several new functions. multiply() is the recursive function which
    obtains a, b, c, and d from a helper function splitIntArray,
    generates the 3 terms of the multiplication equation, and adds them
    together to produce the final product. ArrToInt() was added as a
    helper function for the base case of multiply, when one of the
    terms reaches one digits and the two terms are covnerted to ints
    and multiplied to end the recursion.

--- integer-multiplication/recursive.c
+++ integer-multiplication/recursive.c
@@ -1,57 +1,102 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+#include <stdbool.h>
 
-int* toInt(char * s, int len){
-	int * arr = malloc(sizeof(int) * len);
-	int i = 0;
+int *strToIntArray(char *s, int len){
+	int *intArray = malloc(sizeof(int) * len);
+	int i;
 
-	for(i = 0; i < len; i++) {
-		arr[i] = s[i] - '0';
+	// convert each char to an int
+	for(i = 0; i < len; i++){
+		intArray[i] = s[i] - '0';
 	}
-	
-	return arr;
+
+	return intArray;
 }
 
-int** getabcd(int *arr1, int *arr2, int len1, int len2){
-	int i,j;
-	int *a = malloc(sizeof(int*) * (len1 / 2));
-	int *b = malloc(sizeof(int*) * (len1 / 2 + (len1 % 2)));
-	int *c = malloc(sizeof(int*) * (len2 / 2));
-	int *d = malloc(sizeof(int*) * (len2 / 2 + (len2 % 2)));
-	int **abcd = malloc(sizeof(int*) * 4);
+// populate a new array with half the values from the input array
+int *splitIntArray(int *arr, int len, bool right){
+	int *newArr = malloc(sizeof(int) * len/2);
+	int i;
 
-	for(i = 0; i < len1/2; i++){
-		a[i] = arr1[i];
-		b[i] = arr1[i+len1/2];
+	// 'right' tag means get from second half of input
+	if (right) {
+		for(i = 0; i < len/2; i++) {
+			newArr[i] = arr[i+len/2];
+		} 
 	}
-	if (len1 % 2) {
-		b[i] = arr1[len1 - 1];
-	}
-
-	for(i = 0; i < len2/2; i++){
-		c[i] = arr2[i];
-		d[i] = arr2[i+len2/2];
-	}
-	if (len2 % 2) {
-		d[i] = arr2[len2 - 1];
+	else {
+		for(i = 0; i < len/2; i++) {
+			newArr[i] = arr[i];
+		} 
 	}
 
-	abcd = (int*[4]){a,b,c,d};
+	return newArr;
+}
 
-	return abcd;
+// convert int arrays back into integers
+int arrToInt(int *arr, int len) {
+	int i, j, tens;
+	int result = 0;
+	
+	for(i = 0; i<len; i++){
+		tens = 1;
+		for(j = 0; j < i; j++){
+			tens *= 10;
+		}
+		result += arr[len - 1 - i] * tens;
+	}
+	return result;
+}
+
+int multiply(int *n1, int len1, int *n2, int len2) {
+	int n, term1, term2, term3;
+	int *a, *b, *c, *d;
+
+	// base case
+	if (len1 == 1 || len2 == 1){
+		return arrToInt(n1, len1) * arrToInt(n2, len2);
+	}
+
+	// split each input term in 2 and generate the 3 terms of the multiplication equation
+	else {
+		n = len1 > len2 ? len1 : len2;
+		
+		a = splitIntArray(n1, len1, 0);
+		b = splitIntArray(n1, len1, 1);
+		c = splitIntArray(n2, len2, 0);
+		d = splitIntArray(n2, len2, 1);
+		
+		term1 = pow(10, n) * multiply(a, len1/2, c, len2/2);
+		term2 = pow(10, n/2) * (multiply(a, len1/2, d, len2/2) + multiply(b, len1/2, c, len2/2));
+		term3 = multiply(b, len1/2, d, len2/2);
+
+		free(d);
+		free(c);
+		free(b);
+		free(a);
+
+		return term1 + term2 + term3;
+	}
 }
 
 int main(int argc, char *argv[]) {
-	int n1_len = strlen(argv[1]);
-	int n2_len = strlen(argv[2]);
+	int len1 = strlen(argv[1]);
+	int len2 = strlen(argv[2]);
+	
+	int *n1 = strToIntArray(argv[1], len1);
+	int *n2 = strToIntArray(argv[2], len2);
+	
+	int i, j;
 
-	int * n1 = toInt(argv[1], n1_len);
-	int * n2 = toInt(argv[2], n2_len);
-	int i,j;
-	int **abcd = getabcd(n1, n2, n1_len, n2_len);
+	int product = multiply(n1, len1, n2, len2);
 
-	free(n2);
+	printf("%d\n", product);
+
 	free(n1);
+	free(n2);
+	
 	return 0;
 }
