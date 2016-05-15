cb8bf92ab2f71767de8b471992d79131e4dde9a1
Tim Pellissier <timpellissier@gmail.com>
2016-05-15 10:18:48 -0500

    Add length parameter to sort and remove semicolons
    The sort function requires a length parameter, so the function
    declaration and the initial call were modified to reflect that.
    A length of just 1 element represents the base case of the
    recursion, so the function simply returns in this case.
    
    Also I forgot how python works for a minute there and included a
    bunch of unecessary semicolons. Those are gone now

--- quicksort/quicksort.py
+++ quicksort/quicksort.py
@@ -1,6 +1,9 @@
-def sort(arr):
-	return arr;
+def sort(arr, length):
+	if length == 1:
+		return
+	return (arr, length)
 
 if __name__ == '__main__':
-	unsorted = list(reversed(range(1000)));
-	print sort(unsorted);
+	unsorted = list(reversed(range(1000)))
+	initial_len = len(unsorted)
+	print sort(unsorted, initial_len)
