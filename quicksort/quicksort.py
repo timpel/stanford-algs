5a4da6cb204fea4dde5e522163d5b0f476e17b4f
Tim Pellissier <timpellissier@gmail.com>
2016-05-15 10:27:29 -0500

    Add choose_pivot function (naive)
    choose_pivot() simply returns the first element of the array
    to act as the pivot. Ultimately the pivot will be a randomly
    chosen element as this will yield the best performance on average.

--- quicksort/quicksort.py
+++ quicksort/quicksort.py
@@ -1,9 +1,21 @@
 def sort(arr, length):
+	
 	if length == 1:
 		return
-	return (arr, length)
+
+	pivot = choose_pivot(arr, length)
+
+	return (arr, length, pivot)
+
+
+def choose_pivot(arr, length):
+
+	return arr[0]
+
 
 if __name__ == '__main__':
+	
 	unsorted = list(reversed(range(1000)))
 	initial_len = len(unsorted)
+	
 	print sort(unsorted, initial_len)
