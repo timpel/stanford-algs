de279b84f46e0a7aaecfc701cbd52879a06c6bcb
Tim Pellissier <timpellissier@gmail.com>
2016-05-15 16:13:40 -0500

    Add mergesort/quicksort test script
    This script compares the runtimes of merge sort and quicksort for
    random arrays of exponentially increasing sizes. The max size is
    passed as a command line argument.
    
    The test shows that quicksort currently runs slower than merge
    sort, though it grows at an equivalent rate.

---
+++ sorting_test.py
@@ -0,0 +1,29 @@
+import mergesort.merge_sort
+import quicksort.quicksort
+import sys
+import time
+from random import randint
+
+def main(max_len):
+
+	for n in [2**(n+1) for n in range(max_len)]:
+		print 'Array size: %d' % n
+		arr = [randint(0, 2**max_len) for n in range(n)]
+		
+		current_time = time.time()
+		mergesort.merge_sort.sort(arr)
+		print 'Merge sort: %f' % (time.time() - current_time)
+
+		current_time = time.time()
+		quicksort.quicksort.sort(arr, 0, len(arr))
+		print 'Quicksort: %f' % (time.time() - current_time)
+		print '-----------------'
+
+if __name__ == '__main__':
+
+	try:
+		max_len = int(sys.argv[1])
+	except (IndexError, ValueError):
+		print 'Format: python sorting_test.py <log(max input)>'
+
+	main(max_len)
