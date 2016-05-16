d2a4d84d07e8e481672a55c7031ae7c76e95601d
Tim Pellissier <timpellissier@gmail.com>
2016-05-15 20:51:42 -0500

    DRY up sorting_test by passing in functions
    Now that mergesort and quicksort take exactly the same arguments,
    the fixed-time tests can be written just once using a parameter as
    the function and passing the reference to mergesort and quicksort
    as arguments. This eliminates a bunch of repeated lines.

--- sorting_test.py
+++ sorting_test.py
@@ -15,11 +15,11 @@ def multi_size(max_len):
 		print 'Merge sort: %f' % (time.time() - current_time)
 
 		current_time = time.time()
-		quicksort.quicksort.check(quicksort.quicksort.sort(arr, 0, n+1), n+1)
+		quicksort.quicksort.check(quicksort.quicksort.sort(arr), n+1)
 		print 'Quicksort: %f' % (time.time() - current_time)
 		print '-----------------'
 
-def fixed_time(sec, length):
+def fixed_time(sec, length, sort_func, func_name):
 
 	count = 0
 	start = time.time()
@@ -27,27 +27,15 @@ def fixed_time(sec, length):
 
 	while time.time() < end:
 		arr = [randint(0, length) for n in range(length)]
-		mergesort.merge_sort.sort(arr)
+		sort_func(arr)
 		count += 1
 
-	print 'Merge sort: %d %d-element arrays in %d seconds' % (count, length, sec)
-
-
-	count = 0
-	start = time.time()
-	end = start + sec
-
-	while time.time() < end:
-		arr = [randint(0, length) for n in range(length)]
-		quicksort.quicksort.sort(arr, 0, length)
-		count += 1
-
-	print 'Quicksort: %d %d-element arrays in %d seconds' % (count, length, sec)
-
+	print '%s sort: %d %d-element arrays in %d seconds' % (func_name, count, length, sec)
 
 if __name__ == '__main__':
 
 	if len(sys.argv) > 2:
-		fixed_time(int(sys.argv[1]), int(sys.argv[2]))
+		fixed_time(int(sys.argv[1]), int(sys.argv[2]), mergesort.merge_sort.sort, 'Merge')
+		fixed_time(int(sys.argv[1]), int(sys.argv[2]), quicksort.quicksort.sort, 'Quick')
 	else:
 		multi_size(int(sys.argv[1]))
