7f9fc499adfb0540e43f7f6f049bfc7aeb11ada5
Tim Pellissier <timpellissier@gmail.com>
2016-05-16 23:01:32 -0500

    Only check correctness if instructed
    Added a 'check' parameter which is given the value True if any
    command line argument is given other than the array length.
    The main function now only checks for correctness with quicksort if
    this check parameter is ture; otherwise it simply runs random_select
    and reports the time it took, allowing the testing of much larger
    arrays (up to ~64000000 elements in under a minute).

--- selection_test.py
+++ selection_test.py
@@ -8,22 +8,30 @@ def main(max_len, check):
 	for n in [2**(n+1) for n in range(max_len)]:
 		arr = [randint(0, 2**max_len) for n in range(n)]
 
-		median = int((len(arr)+1)/2) - 1
+		median = int(((n+1)+1)/2) - 1
 
 		current_time = time.time()
 		result = random_selection.random_selection.select(arr, median)
 		end_time = time.time() - current_time
 
-		sorted_arr = quicksort.quicksort.sort(arr)
+		if check:
+			sorted_arr = quicksort.quicksort.sort(arr)
 
-		if sorted_arr[median] == result:
-			print "Success! In %f" % end_time
+			if sorted_arr[median] == result:
+				print "Success! Length: %d -- Time: %f" % (n+1, end_time)
+
+			else:
+				print "Failed"
 
 		else:
-			print "Failed"
+			print "Length: %d -- Time: %f" % (n+1, end_time)
 
 	return
 
 if __name__ == '__main__':
 		arr_len = int(sys.argv[1])
-		main(arr_len)
+
+		if len(sys.argv) > 2:
+			main(arr_len, True)
+		else:
+			main(arr_len, False)
